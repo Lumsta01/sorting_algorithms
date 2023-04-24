36d2809176bac71ec259c55d66610db5f84f43db
Lumsta01 <lulamamanganyi01@gmail.com>
2023-04-24 07:00:34 -0700

    task done

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,37 +1,47 @@
 #include "sort.h"
 
 /**
- * selection_sort - sort array in ascendin order section sort
- * @array: array of integers
- * @size: size of an array
- * Return: void
+ * swap_ints - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
+ */
+
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * selection_sort - Sort an array of integers in ascending order
+ *                  using the selection sort algorithm.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ *
+ * Description: Prints the array after each swap.
  */
 
 void selection_sort(int *array, size_t size)
 {
-	int i, j, min_index;
+	int *min;
+	size_t i, j;
+
+	if (array == NULL || size < 2)
+		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
-		min_index = i;
+		min = array + i;
+		for (j = i + 1; j < size; j++)
+			min = (array[j] < *min) ? (array + j) : min;
 
-		for (j = i + 1; j < n; j++)
+		if ((array + i) != min)
 		{
-			if (array[j] < array[min_index])
-			{
-				min_index = j;
-			}
+			swap_ints(array + i, min);
+			print_array(array, size);
 		}
-
-		int temp = array[min_index];
-		array[min_index] = array[i];
-		array[i] = temp;
-
-		printf("sorted array: ");
-		for (size_t k = 0; k < n; k++)
-			printf("%d ", array[k]);
 	}
-	printf("\n");
 }
-
-
